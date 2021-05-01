40f50fe31cccb9a995de1e0cce1b0ded676efd75
qrvd <36740654+qrvd@users.noreply.github.com>
2021-05-01 12:12:31 +0200

    count users

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const roomData = {};
+
 io.on('connection', (socket) => {
   socket.emit('chat message', "[Please wait until you are connected...]")
   const state = {};
   socket.once('cohort_id', cohort => {
     state.cohort = cohort;
     socket.join(state.cohort);
-    io.in(state.cohort).emit('chat message', "A new user has connected.");
-    socket.emit('chat message', "[You have been connected to cohort " + `${cohort}.]`)
+    if (!!roomData[cohort]) {
+      roomData[cohort].numUsers += 1
+    } else {
+      roomData[cohort] = { numUsers: 1 };
+    }
+    io.in(state.cohort).emit('chat message', `[A new user has joined.]`);
+    socket.emit('chat message', "[You have been connected to the chat room for cohort " + `${cohort}.]`);
+    socket.emit('chat message', `[There are ${roomData[cohort].numUsers} users in this chat room.]`);
   })
   socket.on('chat message', msg => {
     if (!!state.cohort) {
@@ -23,6 +31,11 @@ io.on('connection', (socket) => {
     } else {
       socket.emit('chat message', "[Message could not be sent.]")
     }
+  });
+
+  socket.on('disconnect', () => {
+    if (!state.cohort) return
+    roomData[state.cohort].numUsers -= 1
   });
 });
 
